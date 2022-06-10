bba2ecc8c5401fe859d8c2a370b6a63f80332c25
pawel <suracki.lpGmail.com>
2022-06-10 08:44:12 +0200

    Removed pinia bug

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,10 +1,8 @@
 import { useMainStore } from '@/store/useMainStore';
 import { storeToRefs } from 'pinia';
 
-const { tokenStore } = storeToRefs(useMainStore())
-
 export default defineNuxtRouteMiddleware((to, from) => {
-
+    const { tokenStore } = storeToRefs(useMainStore())
     if (to.name === 'login' && tokenStore.value) {
         return navigateTo('/panel')
     }
